07feebbb51319effa4b74bf717b0c095f614bf40
Gregory McWhirter <greg@ideafreemonoid.org>
2018-08-24 12:58:52 -0700

    Clean up easyjson generate call

--- discordapi/jsonapi/gateway_response.go
+++ discordapi/jsonapi/gateway_response.go
@@ -1,11 +1,11 @@
 package jsonapi
 
-//go:generate easyjson -all -snake_case $GOPATH/src/github.com/gsmcwhirter/discord-bot-lib/discordapi/jsonapi/gateway_response.go
+//go:generate easyjson -all
 
 // GatewayResponse is the json object received from the discord api
 // when requesting gateway connection information
 //easyjson:json
 type GatewayResponse struct {
-	URL    string
-	Shards int
+	URL    string `json:"url"`
+	Shards int    `json:"shards"`
 }
