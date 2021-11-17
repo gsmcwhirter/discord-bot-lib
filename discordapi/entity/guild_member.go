b9595f42d89f550625c67fe8eaeff04e4c7451a7
Gregory McWhirter <greg@ideafreemonoid.org>
2021-11-17 11:08:10 -0500

    Add a bit more error detail in case that doesn't work

--- discordapi/entity/guild_member.go
+++ discordapi/entity/guild_member.go
@@ -83,13 +83,13 @@ func GuildMemberFromElement(e etfapi.Element) (GuildMember, error) {
 	e2 = eMap["deaf"]
 	m.Deaf, err = e2.ToBool()
 	if err != nil {
-		return m, errors.Wrap(err, "could not inflate Deaf")
+		return m, errors.Wrap(err, "could not inflate Deaf", "element_type", e2.Code.String())
 	}
 
 	e2 = eMap["mute"]
 	m.Mute, err = e2.ToBool()
 	if err != nil {
-		return m, errors.Wrap(err, "could not inflate Mute")
+		return m, errors.Wrap(err, "could not inflate Mute", "element_type", e2.Code.String())
 	}
 
 	e2, ok = eMap["roles"]
